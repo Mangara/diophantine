92a7579226b683896d173cf22c7b590c5fa5cceb
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2022-08-07 17:54:37 -0400

    Add no solution iterator

---
+++ src/main/java/com/github/mangara/diophantine/EmptyIterator.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2022 Sander Verdonschot <sander.verdonschot at gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.mangara.diophantine;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * An iterator with no elements.
+ * <p>
+ * This is returned when an equation has no (integer) solutions.
+ */
+public class EmptyIterator implements Iterator<XYPair> {
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public XYPair next() {
+        throw new NoSuchElementException("There are no solutions");
+    }
+
+}
