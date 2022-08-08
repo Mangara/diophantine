0d933d6a90c9ec02b28968f4c76509b4d6f34a44
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2022-08-07 21:29:38 -0400

    Add attribution for quadratic solver

--- src/main/java/com/github/mangara/diophantine/QuadraticSolver.java
+++ src/main/java/com/github/mangara/diophantine/QuadraticSolver.java
@@ -15,9 +15,38 @@
  */
 package com.github.mangara.diophantine;
 
+import java.util.Iterator;
+
 /**
+ * Solves linear Diophantine equations.
+ * <p>
+ * This class allows you to solve Diophantine equations of the form
  *
+ * \[a x^2 + b xy + c y^2 + d x + e y + f = 0\]
+ *
+ * The methods are based on a note and papers by Keith Matthews:
+ * 
+ * <ul>
+ * <li> Solving the Diophantine equation \(ax^2 + bxy + cy^2 + dx + ey + f = 0\), http://www.numbertheory.org/PDFS/general_quadratic_solution.pdf
+ * <li> The Diophantine equation \(ax^2 + bxy + cy^2 = N\), \(D = b^2 - 4ac > 0\), Journal de Théorie des Nombres de Bordeaux, tome 14, no 1 (2002), p. 257-270, http://www.numdam.org/item/?id=JTNB_2002__14_1_257_0
+ * <li> Lagrange's Algorithm revisited: solving \(at^2 + btu + cu^2 = n\) in the case of negative discriminant, Journal of Integer Sequences, Vol. 17 (2014), Article 14.11.1, https://cs.uwaterloo.ca/journals/JIS/VOL17/Matthews/matt10.pdf
+ * <li> On solving a binary quadratic Diophantine equation (co-authored with John P. Robertson), Rocky Mountain J. Math. 51(4): 1369-1385 (August 2021). DOI: 10.1216/rmj.2021.51.1369
+ * </ul>
  */
 public class QuadraticSolver {
-    
+
+    /**
+     * Solves the quadratic Diophantine equation a x^2 + b xy + c y^2 + d x + e y + f = 0.
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @param d
+     * @param e
+     * @param f
+     * @return an iterator over all integer solutions (x, y)
+     */
+    public static Iterator<XYPair> solve(int a, int b, int c, int d, int e, int f) {
+        return new EmptyIterator();
+    }
 }
