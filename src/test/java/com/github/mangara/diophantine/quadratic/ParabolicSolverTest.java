594a135f8fa1e5c0dfdcefd6379ce85bf86fddc8
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2022-08-15 22:50:16 -0400

    Solve a = 0 case

--- src/test/java/com/github/mangara/diophantine/quadratic/ParabolicSolverTest.java
+++ src/test/java/com/github/mangara/diophantine/quadratic/ParabolicSolverTest.java
@@ -303,8 +303,7 @@ public class ParabolicSolverTest {
         };
 
         TestUtils.validateExpectedSolutions(a, b, c, d, e, f, expectedSolutions);
-        TestUtils.assertNotSupportedYet(() -> { ParabolicSolver.solve(a, b, c, d, e, f); });
-        //TestUtils.assertSolutionsInclude(a, b, c, d, e, f, expectedSolutions, ParabolicSolver.solve(a, b, c, d, e, f));
+        TestUtils.assertSolutionsInclude(a, b, c, d, e, f, expectedSolutions, ParabolicSolver.solve(a, b, c, d, e, f));
     }
     
     @Test
