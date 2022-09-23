2139f48fe7f3ac08ae2fbbec26b3f171d1b11ca1
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2022-09-23 10:41:10 -0400

    Switch SquareDiscriminantSolver to BigInteger

--- src/main/java/com/github/mangara/diophantine/quadratic/SquareDiscriminantSolver.java
+++ src/main/java/com/github/mangara/diophantine/quadratic/SquareDiscriminantSolver.java
@@ -21,6 +21,7 @@ import com.github.mangara.diophantine.XYPair;
 import com.github.mangara.diophantine.iterators.EmptyIterator;
 import com.github.mangara.diophantine.iterators.MergedIterator;
 import com.github.mangara.diophantine.utils.Divisors;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -45,8 +46,25 @@ public class SquareDiscriminantSolver {
      * @param f
      * @return an iterator over all integer solutions (x, y)
      */
-    public static Iterator<XYPair> solve(int a, int b, int c, int d, int e, int f) {
-        if (a == 0) {
+    public static Iterator<XYPair> solve(long a, long b, long c, long d, long e, long f) {
+        return solve(BigInteger.valueOf(a), BigInteger.valueOf(b), BigInteger.valueOf(c), BigInteger.valueOf(d), BigInteger.valueOf(e), BigInteger.valueOf(f));
+    }
+    
+    /**
+     * Solves the quadratic Diophantine equation
+     * a x^2 + b xy + c y^2 + d x + e y + f = 0,
+     * given that D = b^2 - 4ac = g^2, for g > 0.
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @param d
+     * @param e
+     * @param f
+     * @return an iterator over all integer solutions (x, y)
+     */
+    public static Iterator<XYPair> solve(BigInteger a, BigInteger b, BigInteger c, BigInteger d, BigInteger e, BigInteger f) {
+        if (a.signum() == 0) {
             return solveZeroA(a, b, c, d, e, f);
         } else {
             return solveNonZeroA(a, b, c, d, e, f);
@@ -54,28 +72,28 @@ public class SquareDiscriminantSolver {
     }
 
     // Pre: D = b^2 - 4ac = g^2, g > 0 && a == 0
-    private static Iterator<XYPair> solveZeroA(int a, int b, int c, int d, int e, int f) {
+    private static Iterator<XYPair> solveZeroA(BigInteger a, BigInteger b, BigInteger c, BigInteger d, BigInteger e, BigInteger f) {
         // Legendre's transformation with Dx = X + alpha and Dy = Y + beta gives
         //  aX^2 + bXY + cY^2 = k
         // With a = 0, this reduces to
         //  Y(bX + cY) = k
         
-        long D = Utils.discriminant(a, b, c);
-        long alpha = Utils.legendreAlpha(b, c, d, e);
-        long beta = Utils.legendreBeta(a, b, d, e);
-        long k = Utils.legendreConstant(a, b, c, d, e, f, D);
-        long h = Utils.gcd(b, c);
+        BigInteger D = Utils.discriminant(a, b, c);
+        BigInteger alpha = Utils.legendreAlpha(b, c, d, e);
+        BigInteger beta = Utils.legendreBeta(a, b, d, e);
+        BigInteger k = Utils.legendreConstant(a, b, c, d, e, f, D);
+        BigInteger h = b.gcd(c);
         
-        if (k == 0) {
+        if (k.signum() == 0) {
             // Y(bX + cY) = 0, so 
             // Y = 0  or  bX + cY = 0
             
             // Dy - beta = 0  =>  Dy = beta
-            Iterator<XYPair> yZero = LinearSolver.solve(0, D, Math.negateExact(beta));
+            Iterator<XYPair> yZero = LinearSolver.solve(BigInteger.ZERO, D, beta.negate());
             
             // b(Dx - alpha) + c(Dy - beta) = 0  =>  bDx + cDy = b alpha + c beta
-            long t = Math.addExact(Math.multiplyExact(b, alpha), Math.multiplyExact(c, beta));
-            Iterator<XYPair> yNonZero = LinearSolver.solve(Math.multiplyExact(b, D), Math.multiplyExact(c, D), Math.negateExact(t));
+            BigInteger t = b.multiply(alpha).add(c.multiply(beta));
+            Iterator<XYPair> yNonZero = LinearSolver.solve(b.multiply(D), c.multiply(D), t.negate());
             
             return MergedIterator.merge(yZero, yNonZero);
         } else {
@@ -83,25 +101,25 @@ public class SquareDiscriminantSolver {
             // We need to examine all divisors d_i of k/h and solve the system
             //  Y = d_i  and  (b/h)X + (c/h)Y = k/(h d_i)
             
-            long b2 = b / h;
-            long c2 = c / h;
-            long k2 = k / h;
+            BigInteger b2 = b.divide(h);
+            BigInteger c2 = c.divide(h);
+            BigInteger k2 = k.divide(h);
             
             List<XYPair> solutions = new ArrayList<>();
-            List<Long> divisors = Divisors.getPositiveAndNegativeDivisors(k2);
+            List<Long> divisors = Divisors.getPositiveAndNegativeDivisors(k2.longValueExact());
             
             for (Long divisor : divisors) {
-                long Y = divisor;
-                long Xtop = Math.subtractExact(k2 / divisor, Math.multiplyExact(c2, Y));
+                BigInteger Y = BigInteger.valueOf(divisor);
+                BigInteger Xtop = k2.divide(BigInteger.valueOf(divisor)).subtract(c2.multiply(Y));
                 
-                if (Xtop % b2 == 0) {
-                    long X = Xtop / b2;
+                if (Xtop.mod(b2.abs()).signum() == 0) {
+                    BigInteger X = Xtop.divide(b2);
                     
-                    long xD = Math.addExact(X, alpha);
-                    long yD = Math.addExact(Y, beta);
+                    BigInteger xD = X.add(alpha);
+                    BigInteger yD = Y.add(beta);
 
-                    if (xD % D == 0 && yD % D == 0) {
-                        solutions.add(new XYPair(xD / D, yD / D));
+                    if (xD.mod(D).signum() == 0 && yD.mod(D).signum() == 0) {
+                        solutions.add(new XYPair(xD.divide(D), yD.divide(D)));
                     }
                 }
             }
@@ -111,51 +129,51 @@ public class SquareDiscriminantSolver {
     }
 
     // Pre: D = b^2 - 4ac = g^2, g > 0 && a != 0
-    private static Iterator<XYPair> solveNonZeroA(int a, int b, int c, int d, int e, int f) {
+    private static Iterator<XYPair> solveNonZeroA(BigInteger a, BigInteger b, BigInteger c, BigInteger d, BigInteger e, BigInteger f) {
         // Legendre's transformation with Dx = X + alpha and Dy = Y + beta gives
         //  aX^2 + bXY + cY^2 = k
         // After multiplying by 4a, we can rewrite this as
         //  (2aX + (b + g)Y)(2aX + (b - g)Y) = 4ak
 
-        long D = Utils.discriminant(a, b, c);
-        long g = Math.round(Math.sqrt(D));
-        long k = Utils.legendreConstant(a, b, c, d, e, f, D);
-        long fourAK = Math.multiplyExact(4L * a, k);
+        BigInteger D = Utils.discriminant(a, b, c);
+        BigInteger g = D.sqrt();
+        BigInteger k = Utils.legendreConstant(a, b, c, d, e, f, D);
+        BigInteger fourAK = BigInteger.valueOf(4).multiply(a).multiply(k);
 
-        long g1 = Utils.gcd(Math.multiplyExact(2L, a), Math.addExact(b, g));
-        long g2 = Utils.gcd(Math.multiplyExact(2L, a), Math.subtractExact(b, g));
-        long g1g2 = Math.multiplyExact(g1, g2);
+        BigInteger g1 = BigInteger.TWO.multiply(a).gcd(b.add(g));
+        BigInteger g2 = BigInteger.TWO.multiply(a).gcd(b.subtract(g));
+        BigInteger g1g2 = g1.multiply(g2);
 
-        long alpha = Utils.legendreAlpha(b, c, d, e);
-        long beta = Utils.legendreBeta(a, b, d, e);
+        BigInteger alpha = Utils.legendreAlpha(b, c, d, e);
+        BigInteger beta = Utils.legendreBeta(a, b, d, e);
 
         // This reduces to
         //  ((2a / g1)X + ((b + g) / g1)Y)((2a / g2)X + ((b - g) / g2)Y) = 4ak / g1g2
-        if (k == 0) {
+        if (k.signum() == 0) {
             // If k = 0 we can solve both parts separately
             //  2aX + (b + g)Y = 0  or  2aX + (b - g)Y = 0
             // Subsituting X = Dx - alpha and Y = Dy - beta gives
             //  2aDx + (b + g)Dy = 2a alpha + (b + g) beta  or
             //  2aDx + (b - g)Dy = 2a alpha + (b - g) beta
 
-            long Dg1 = Math.multiplyExact(D, g1);
-            long right1 = Math.addExact(
-                    Math.multiplyExact(2L * a, alpha),
-                    Math.multiplyExact(Math.addExact(b, g), beta));
+            BigInteger Dg1 = D.multiply(g1);
+            BigInteger right1 = BigInteger.TWO.multiply(a).multiply(alpha).add(
+                    b.add(g).multiply(beta)
+            );
 
             Iterator<XYPair> eq1 = new EmptyIterator<>();
-            if (right1 % Dg1 == 0) {
-                eq1 = LinearSolver.solve(2L * a / g1, Math.addExact(b, g) / g1, Math.negateExact(right1) / Dg1);
+            if (right1.mod(Dg1.abs()).signum() == 0) {
+                eq1 = LinearSolver.solve(BigInteger.TWO.multiply(a).divide(g1), b.add(g).divide(g1), right1.negate().divide(Dg1));
             }
 
-            long Dg2 = Math.multiplyExact(D, g2);
-            long right2 = Math.addExact(
-                    Math.multiplyExact(2L * a, alpha),
-                    Math.multiplyExact(Math.subtractExact(b, g), beta));
+            BigInteger Dg2 = D.multiply(g2);
+            BigInteger right2 = BigInteger.TWO.multiply(a).multiply(alpha).add(
+                    b.subtract(g).multiply(beta)
+            );
 
             Iterator<XYPair> eq2 = new EmptyIterator<>();
-            if (right2 % Dg2 == 0) {
-                eq2 = LinearSolver.solve(2L * a / g2, Math.subtractExact(b, g) / g2, Math.negateExact(right2) / Dg2);
+            if (right2.mod(Dg2.abs()).signum() == 0) {
+                eq2 = LinearSolver.solve(BigInteger.TWO.multiply(a).divide(g2), b.subtract(g).divide(g2), right2.negate().divide(Dg2));
             }
 
             return MergedIterator.merge(eq1, eq2);
@@ -165,7 +183,7 @@ public class SquareDiscriminantSolver {
             // to see if we can solve the system T1 = d_i, T2 = 4ak / g1g2d_i
 
             List<XYPair> solutions = new ArrayList<>();
-            List<Long> divisors = Divisors.getPositiveAndNegativeDivisors(fourAK / g1g2);
+            List<Long> divisors = Divisors.getPositiveAndNegativeDivisors(fourAK.divide(g1g2).longValueExact());
 
             for (Long divisor : divisors) {
                 // We need to solve
@@ -174,26 +192,27 @@ public class SquareDiscriminantSolver {
                 // Adding (g2 / g1) times (2) to (1) gives
                 //  (2g / g1) Y = divisor - 4ak / (g1^2 divisor)
                 //            Y = ((g1 divisor)^2 - 4ak) / (2g g1 divisor)
+                BigInteger div = BigInteger.valueOf(divisor);
+                
+                BigInteger Ytop = g1.multiply(g1).multiply(div.multiply(div)).subtract(fourAK);
+                BigInteger Ybottom = BigInteger.TWO.multiply(g).multiply(g1).multiply(div);
 
-                long Ytop = Math.subtractExact(Math.multiplyExact(Math.multiplyExact(g1, g1), Math.multiplyExact(divisor, divisor)), fourAK);
-                long Ybottom = Math.multiplyExact(Math.multiplyExact(2, g), Math.multiplyExact(g1, divisor));
-
-                if (Ytop % Ybottom == 0) {
-                    long Y = Ytop / Ybottom;
+                if (Ytop.mod(Ybottom.abs()).signum() == 0) {
+                    BigInteger Y = Ytop.divide(Ybottom);
 
                     // Subsituting into (2a / g1)X + ((b + g) / g1)Y = divisor gives
                     //  X = (g1 divisor - (b + g) Y) / 2a
-                    long Xtop = Math.subtractExact(Math.multiplyExact(g1, divisor), Math.multiplyExact(Math.addExact(b, g), Y));
-                    long Xbottom = Math.multiplyExact(2, a);
+                    BigInteger Xtop = g1.multiply(div).subtract(b.add(g).multiply(Y));
+                    BigInteger Xbottom = BigInteger.TWO.multiply(a);
 
-                    if (Xtop % Xbottom == 0) {
-                        long X = Xtop / Xbottom;
+                    if (Xtop.mod(Xbottom.abs()).signum() == 0) {
+                        BigInteger X = Xtop.divide(Xbottom);
+                        
+                        BigInteger xD = X.add(alpha);
+                        BigInteger yD = Y.add(beta);
 
-                        long xD = Math.addExact(X, alpha);
-                        long yD = Math.addExact(Y, beta);
-
-                        if (xD % D == 0 && yD % D == 0) {
-                            solutions.add(new XYPair(xD / D, yD / D));
+                        if (xD.mod(D).signum() == 0 && yD.mod(D).signum() == 0) {
+                            solutions.add(new XYPair(xD.divide(D), yD.divide(D)));
                         }
                     }
                 }
