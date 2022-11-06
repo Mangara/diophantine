cebdfdbeefde3bbc0b09819e33e1abca6c17274d
Sander Verdonschot <sander.verdonschot+github@gmail.com>
2022-11-06 16:53:25 -0500

    Add initial Florida transform code

--- src/main/java/com/github/mangara/diophantine/quadratic/HyperbolicSolver.java
+++ src/main/java/com/github/mangara/diophantine/quadratic/HyperbolicSolver.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.github.mangara.diophantine.quadratic;
 
+import com.github.mangara.diophantine.Utils;
 import com.github.mangara.diophantine.XYPair;
 import java.math.BigInteger;
 import java.util.Iterator;
@@ -23,7 +23,7 @@ import java.util.Iterator;
 public class HyperbolicSolver {
 
     /**
-     * Solves the quadratic Diophantine equation 
+     * Solves the quadratic Diophantine equation
      * a x^2 + b xy + c y^2 + d x + e y + f = 0,
      * given that D = b^2 - 4ac > 0 and not a perfect square.
      *
@@ -38,9 +38,9 @@ public class HyperbolicSolver {
     public static Iterator<XYPair> solve(long a, long b, long c, long d, long e, long f) {
         return solve(BigInteger.valueOf(a), BigInteger.valueOf(b), BigInteger.valueOf(c), BigInteger.valueOf(d), BigInteger.valueOf(e), BigInteger.valueOf(f));
     }
-    
+
     /**
-     * Solves the quadratic Diophantine equation 
+     * Solves the quadratic Diophantine equation
      * a x^2 + b xy + c y^2 + d x + e y + f = 0,
      * given that D = b^2 - 4ac > 0 and not a perfect square.
      *
@@ -56,7 +56,49 @@ public class HyperbolicSolver {
         if (d.signum() == 0 && e.signum() == 0) {
             return RestrictedHyperbolicSolver.solve(a, b, c, f);
         }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        if (a.signum() > 0 && a.gcd(b).gcd(c).gcd(d).gcd(e).equals(BigInteger.ONE)) {
+            return solveWithFloridaTransform(a, b, c, d, e, f);
+        }
+
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    // Pre-condition: a > 0 && (d != 0 || e != 0) && D > 0 and non-square && ae^2 - bde + cd^2 + fD != 0 && gcd(a, b, c, d, e) = 1
+    private static Iterator<XYPair> solveWithFloridaTransform(BigInteger a, BigInteger b, BigInteger c, BigInteger d, BigInteger e, BigInteger f) {
+        BigInteger D = Utils.discriminant(a, b, c);
+        BigInteger alpha = Utils.legendreAlpha(b, c, d, e);
+        BigInteger beta = Utils.legendreBeta(a, b, d, e);
+
+        BigInteger gcdA = alpha.gcd(D);
+        BigInteger gcdB = beta.gcd(D);
+
+        BigInteger r1 = alpha.divide(gcdA);
+        BigInteger r2 = D.divide(gcdA);
+        BigInteger s1 = beta.divide(gcdB);
+        BigInteger s2 = D.divide(gcdB);
+
+        BigInteger A = a.multiply(s2).multiply(s2); // a * s2 * s2
+        BigInteger B = b.multiply(r2).multiply(s2); // b * r2 * s2;
+        BigInteger C = c.multiply(r2).multiply(r2); // c * r2 * r2;
+
+        // Simplify
+        BigInteger G = A.gcd(B).gcd(C);
+        A = A.divide(G);
+        B = B.divide(G);
+        C = C.divide(G);
+
+        // M = -1 * r2 * r2 * s2 * s2 * (a * e * e - b * d * e + c * d * d + f * D) / (D * G)
+        BigInteger M = r2.multiply(r2).multiply(s2).multiply(s2).multiply(
+                a.multiply(e).multiply(e)
+                        .subtract(b.multiply(d).multiply(e))
+                        .add(c.multiply(d).multiply(d))
+                        .add(f.multiply(D))
+        ).divide(D).divide(G)
+                .negate();
+        
+        BigInteger D2 = Utils.discriminant(A, B, C);
+
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
